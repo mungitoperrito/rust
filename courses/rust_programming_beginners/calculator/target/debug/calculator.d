1b9f1b247f3249863d4d80fe4671f345dbc45609
Dave For Projects <daveforprojects@gmail.com>
2025-03-20 19:57:43 -0400

    Calculator

--- courses/rust_programming_beginners/calculator/target/debug/calculator.d
+++
@@ -1,1 +0,0 @@
-/mnt/e/FILES/projects/rust/courses/rust_programming_beginners/calculator/target/debug/calculator: /mnt/e/FILES/projects/rust/courses/rust_programming_beginners/calculator/src/main.rs
--- courses/rust_programming_beginners/calculator/target/release/calculator.d
+++
@@ -1,1 +0,0 @@
-/mnt/e/FILES/projects/rust/courses/rust_programming_beginners/calculator/target/release/calculator: /mnt/e/FILES/projects/rust/courses/rust_programming_beginners/calculator/src/main.rs
