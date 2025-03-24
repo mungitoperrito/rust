3b613a087bf4d91e6294881e06bf4cdad0db2859
Dave For Projects <daveforprojects@gmail.com>
2025-03-24 14:32:11 -0400

    sizes

--- courses/rust_tutorial/src/main.rs
+++ courses/rust_tutorial/src/main.rs
@@ -7,11 +7,22 @@ use std::io;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 
 
-fn main() {
-    println!("Max u32: {}", u32::MAX);
-    println!("Max u64: {}", u64::MAX);
-    println!("Max usize: {}", usize::MAX);
-    println!("Max f32: {}", f32::MAX);
-    println!("Max f64: {}", f64::MAX);
+// // Uncomment to see type sizes
+// fn main() {
+//     println!("Max u32: {}", u32::MAX);
+//     println!("Max u64: {}", u64::MAX);
+//     println!("Max usize: {}", usize::MAX);
+//     println!("Max f32: {}", f32::MAX);
+//     println!("Max f64: {}", f64::MAX);
+// }
 
+// Uncomment to see floats
+fn main() {
+    let _ignore_this: f32 = 123.45;   // compiler ignores: underscore var name
+
+    let num_01: f32 = 1.111111111111111;
+    println!("f32: {}", num_01 + 0.1111111111111111 );
+    let num_02: f64 = 1.111111111111111;
+    println!("f64: {}", num_02 + 0.1111111111111111 );
 }
+
