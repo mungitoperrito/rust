4a82f312cda2cf3a0a2f301e7a538ac6ef88ee81
Dave For Projects <daveforprojects@gmail.com>
2025-03-24 16:05:36 -0400

    Random

--- courses/rust_tutorial/src/main_03.rs
+++ courses/rust_tutorial/src/main_03.rs
@@ -18,6 +18,16 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 
 // Uncomment to see floats
 fn main() {
+    let _ignore_this: f32 = 123.45;   // compiler ignores: underscore var name
 
+    let num_01: f32 = 1.111111111111111;
+    println!("f32: {}", num_01 + 0.1111111111111111 );
+    let num_02: f64 = 1.111111111111111;
+    println!("f64: {}", num_02 + 0.1111111111111111 );
+
+    let mut plus_plus: u32 = 1;
+
+    // println doesn't like increment at right
+    println!("{} {:?}", plus_plus, plus_plus);
 }
 
