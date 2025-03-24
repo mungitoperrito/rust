247e45ee1533a9bf62430408236f76cbe81b7f18
Dave For Projects <daveforprojects@gmail.com>
2025-03-24 14:07:21 -0400

    main_01.rs

--- courses/rust_tutorial/src/main.rs
+++ courses/rust_tutorial/src/main.rs
@@ -1,3 +1,12 @@
+#![allow(unused)]           // Suppress unused variable warnings
+
+use std::cmp::Ordering;
+use std::fs::File;
+use rand::Rng;
+use std::io;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
