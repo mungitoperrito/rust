f19f9d19929377185d8d5dfe47ccc1e1b3ad8e00
Dave For Projects <daveforprojects@gmail.com>
2025-03-24 16:30:30 -0400

    ternary

--- courses/rust_tutorial/src/main.rs
+++ courses/rust_tutorial/src/main.rs
@@ -7,21 +7,32 @@ use std::io;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 
 
-// Uncomment for rand num between 1, 100 (inclusive)
-fn main() {
-    // Need to learn typing for lists
-    let ages = [0, 8, 18, 19, 21, 35, 50, 65, 80 ];
+// // Uncomment for if .. else if .. else
+// fn main() {
+//     // Need to learn typing for lists
+//     let ages = [0, 8, 18, 19, 21, 35, 50, 65, 80 ];
 
-    for age in ages  {
-        if (age >= 1) && (age <= 18) {
-            println!("{}: Important BDay", age);
-        } else if (age == 21) || (age == 50) {
-            println!("{}: Important BDay", age);
-        } else if (age >= 65) {
-            println!("{}: Important BDay", age);
-        } else {
-            println!("{}: Not important", age);
-        }
-    }
+//     for age in ages  {
+//         if (age >= 1) && (age <= 18) {
+//             println!("{}: Important BDay", age);
+//         } else if (age == 21) || (age == 50) {
+//             println!("{}: Important BDay", age);
+//         } else if (age >= 65) {
+//             println!("{}: Important BDay", age);
+//         } else {
+//             println!("{}: Not important", age);
+//         }
+//     }
+// }
+
+// Uncomment for ternary operator
+fn main(){
+    let mut age: u32 = 47;
+    let mut can_vote: bool = if age >= 18 {
+        true             // Return statement: not 'return', no ;
+    } else {
+        false            // Return statement: not 'return', no ;
+    };
+
+    println!("{}", can_vote);   // Variable needs a format string
 }
-
