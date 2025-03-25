c7ab7c728687e33cdc84282bc51346d9a09cbb5a
Dave Cuthbert <daveforprojects@gmail.com>
2025-03-25 16:22:07 -0400

    Tuples (#15)

--- courses/rust_tutorial/src/main.rs
+++ courses/rust_tutorial/src/main.rs
@@ -7,42 +7,14 @@ use std::io;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 
 
-// Uncomment for arrays
+// Tuples
 fn main(){
-    let array_01: [i32; 4] = [1,2,3,4];
+    let tuple_01: (u8, String, f64) = (47, "Yoda".to_string(), 50000.00);
 
-    // // Uncomment for an array
-    // println!("1st: {}", array_01[0]);
-    // println!("Length: {}", array_01.len());
+    println!("Name: {}", tuple_01.1);  // Reference value
 
-    // // Uncomment for a loop
-    // let array_02: [i32; 9] = [1,2,3,4,5,6,7,8,9];
-    // let mut idx: usize = 0;
-    // loop {
-    //     if array_02[idx] % 2 == 0 {
-    //         println!("{}", array_02[idx]);
-    //         idx += 1;
-    //     } else {
-    //         idx += 1;
-    //         continue;
-    //     }
-    //     if idx == 9 {
-    //         break;
-    //     }
-
-    // }
-
-    // // Uncomment for while loop
-    // let array_02: [i32; 9] = [1,2,3,4,5,6,7,8,9];
-    // let mut idx: usize = 0;
-    // while idx < array_02.len(){
-    //     println!("Array: {}", array_02[idx]);
-    //     idx += 1;
-    // }
-
-
-    // Uncomment for for loop
-    for val in array_01.iter() {      // autocomplete suggestion val: &i32 fails
-        println!("Val: {}", val);
-    }
+    // Type assignment inside the tuple fails
+    // let (val_01: u8, val_02: String, val_03: f64) = tuple_01;
+    let (val_01, val_02, val_03) = tuple_01;      // Works
+    println!("val_03: {}", val_03);
 }
